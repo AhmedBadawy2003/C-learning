dc89f796608748645f7119ef8e1b43e09231640e
AhmedBadawy2003 <168972698+AhmedBadawy2003@users.noreply.github.com>
2024-11-28 20:28:53 +0200

    Update swap numbers with a variable.c

--- swap numbers with a variable.c
+++ swap numbers with a variable.c
@@ -1,1 +1,26 @@
 
+/***** Write a program to swap two numbers using a temporary variable****/
+/*****        name: ahmed mohamed badawy , date: 28/11/2024          ****/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int x,y,z;
+x=5;
+y=3;
+
+printf("x=  %d,y=%d   \n",x,y);
+
+z=x;
+
+x=y;
+
+
+y=z;
+printf("x=  %d,y=%d   \n",x,y);
+
+    return 0;
+}
