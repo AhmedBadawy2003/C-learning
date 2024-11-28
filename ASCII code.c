1612684634291be505ab9441362ccf556119b3c9
Ahmed Mohamed Badawy <168972698+AhmedBadawy2003@users.noreply.github.com>
2024-11-28 21:17:54 +0200

    Create ASCII code.c

---
+++ ASCII code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char character;
+printf("this program shows the ASCII value for the given character!");
+printf("please enter the character:");
+scanf("%c",&character);
+printf("The ASCII value for your character is: %d",character);
+
+
+
+    return 0;
+}
