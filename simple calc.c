2c07855f9d60652d8fb257788a3357c0fda48e56
Ahmed Mohamed Badawy <168972698+AhmedBadawy2003@users.noreply.github.com>
2024-11-28 22:03:27 +0200

    Update simple calc.c

--- simple calc.c
+++ simple calc.c
@@ -16,11 +16,12 @@ sub=x-y;
 mult=x*y;
 div=(float)x/y;
 
-printf("X+Y=%d\n",add);
-printf("X-Y=%d\n",sub);
-printf("X*Y=%d\n",mult);
-printf("X/Y=%.2f\n",div);
+printf("%d+%d=%d\n",x,y,add);
+printf("%d-%d=%d\n",x,y,sub);
+printf("%d*%d=%d\n",x,y,mult);
+printf("%d/%d=%.2f\n",x,y,div);
 
 
     return 0;
 }
+
