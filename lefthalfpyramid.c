bcd736ea9f23da8b7abdb5210eddf0e2245b845f
Ahmed Mohamed Badawy <168972698+AhmedBadawy2003@users.noreply.github.com>
2024-12-14 09:08:08 +0200

    Create lefthalfpyramid.c

---
+++ lefthalfpyramid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, i,y,l;
+    printf("please enter the height of the pyramid:\n");
+    scanf("%d",&x);
+    for(i=1;i<=x;i++)
+{
+     for(l=1;l+i<=5;l++)
+{
+    printf(" ");
+
+}
+    for(y=1;y<=i;y++)
+{
+    printf("*");
+
+}
+
+    printf("\n");
+}
+    return 0;
+}
