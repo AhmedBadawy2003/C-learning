dce2730b575b8a8ceb65c8c4dc8fadec2d75a533
Ahmed Mohamed Badawy <168972698+AhmedBadawy2003@users.noreply.github.com>
2024-12-14 10:25:09 +0200

    Create full_pyramid.c

---
+++ full_pyramid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, i,y,l;
+    printf("please enter the height of the pyramid:\n");
+    scanf("%d",&x);
+    for(i=1;i<=x;i++)
+{
+     for(l=1;l+i<=x;l++)
+{
+    printf(" ");
+
+}
+    for(y=1;y<=(2*i-1);y++)
+{
+    printf("*");
+
+}
+
+    printf("\n");
+}
+    return 0;
+}
